a8863d875265afb1c92b3bbe32afa99d58e3b7d6
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2022-06-01 17:56:52 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NextX</title>
+        <meta name="description" content="Read about anything & everything from nextX Feed." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Welcome to NextX Feed!</p>
+    </div>
+  )
+}
---
+++ setup.md
@@ -0,0 +1,3 @@
+Create NextJs Project  
+`npx create-next-app@latest next-x-feed --use-npm`
+
